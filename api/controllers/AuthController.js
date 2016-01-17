8215260538f2ddaf088c18e1a6e061636ce265fd
Jason Huntrods <jason@iastate.edu>
2016-01-17 17:29:23 -0600

    Update AuthController.js
    basic flow

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -5,7 +5,28 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var twitterAPI = require('node-twitter-api');
+
 module.exports = {
+	
+	connect: {
+		
+	var twitter = new twitterAPI({
+	    consumerKey: 'ppu9vZOIZMIcA6ZcUflQZHiy7',
+	    consumerSecret: 'XvS1jya2OgbFEWmM7bPEDlBhZVw4Pw3IPQ4kXgVj6N0wkIAgWl',
+	    callback: 'http://104.131.2.65/'
+	});
+	twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results) {
+		if(error) {
+			console.log('Error getting OAuth request token : ' + error);
+		} else {
+			console.log(requestToken);
+			console.log(requestTokenSecret);
+			console.log(results);
+		}
+	});
+	
+	}
 	
 };
 
