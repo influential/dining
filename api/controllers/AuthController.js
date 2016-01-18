500094a37e9d17f37ee8b598356847f69e942ec2
Jason Huntrods <jason@iastate.edu>
2016-01-17 18:01:35 -0600

    Update AuthController.js
    test

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -16,24 +16,27 @@ module.exports = {
 	    consumerSecret: 'XvS1jya2OgbFEWmM7bPEDlBhZVw4Pw3IPQ4kXgVj6N0wkIAgWl',
 	    callback: 'http://104.131.2.65/'
 	});
-	twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results) {
-		if(error) {
-			console.log('Error getting OAuth request token : ' + error);
-		} else {
-			console.log(requestToken);
-			console.log(requestTokenSecret);
-			console.log(results);
-			twitter.getAccessToken(requestToken, requestTokenSecret, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
-				if (error) {
-					console.log(error);
-				} else {
-					console.log(accessToken);
-					console.log(accessTokenSecret);
-					console.log(results);
-				}
-			});
-		}
-	});
+	if(req.param('oauth_verifier')) {
+		twitter.getAccessToken(process.env.RT, process.env.RTS, req.param('oauth_verifier'), function(error, accessToken, accessTokenSecret, results) {
+			if (error) {
+				console.log(error);
+			} else {
+				console.log(accessToken);
+				console.log(accessTokenSecret);
+				console.log(results);
+			}
+		});
+	} else {
+		twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results) {
+			if(error) {
+				console.log('Error getting OAuth request token : ' + error);
+			} else {
+				process.env.RT = requestToken;
+				process.env.RTS = requestTokenSecret;
+				twitter.getAuthUrl(requestToken);
+			}
+		});
+	}
 	
 	}
 	
