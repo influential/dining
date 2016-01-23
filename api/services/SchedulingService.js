8a028b5092502241c0375876cd5950ea430b4421
Jason Huntrods <jason@iastate.edu>
2016-01-23 17:38:52 -0600

    Update SchedulingService.js
    ditching nightmare

--- api/services/SchedulingService.js
+++ api/services/SchedulingService.js
@@ -1,7 +1,5 @@
 var twitterAPI = require('node-twitter-api');
 var async = require('async');
-var Nightmare = require('nightmare');
-var vo = require('vo');
 
 module.exports =  {
   
@@ -37,24 +35,34 @@ module.exports =  {
 	},
   
 	snapshot: function(location, selector) {
-  	    
-  	    console.log("2");
-  	    vo(function* () {
-		  var nightmare = Nightmare({ show: true });
-		  var link = yield nightmare
-		    .goto('http://yahoo.com')
-		    .type('input[title="Search"]', 'github nightmare')
-		    .click('.searchsubmit')
-		    .wait('.ac-21th')
-		    .evaluate(function () {
-		      return document.getElementsByClassName('ac-21th')[0].href;
-		    });
-		  yield nightmare.end();
-		  return link;
-		})(function (err, result) {
-		  if (err) return console.log(err);
-		  console.log(result);
-		});
+		
+		
+  	    /*'../../.tmp/public/' + location + '.png'
+  	    var run = yield nightmare.goto('http://dining.iastate.edu/menus/' + location + '/' + date)
+	    .inject('js', 'node_modules/jquery/dist/jquery.js')
+	    var date = new Date().toISOString().slice(0,10);
+	    var element = $(document).find(selector);
+		    var exists = element.length;
+		    if(exists > 0) {
+		            console.log("ex" + exists);
+			    if(!end) end = exists;
+			    if(!start) start = 0;
+			    var left, right, top, bottom = 0;
+			    for(var i = start; i < end; i++) {
+			      	if(left > element[i].getBoundingClientRect().left) left = element[i].getBoundingClientRect().left;
+			      	if(right < element[i].getBoundingClientRect().right) right = element[i].getBoundingClientRect().right;
+			      	if(top > element[i].getBoundingClientRect().top) top = element[i].getBoundingClientRect().top;
+			      	if(bottom < element[i].getBoundingClientRect().bottom) bottom = element[i].getBoundingClientRect().bottom;
+			    }
+			    window.scrollTo(Math.round(left), Math.round(top));
+			    console.log(top + "-" + left);
+			    return {
+			        x: Math.round(left),
+			        y: Math.round(top),
+			        width: Math.round(right - left),
+			        height: Math.round(bottom - top)
+			    };
+		    }*/
 	},
   
 	tweet: function() {
