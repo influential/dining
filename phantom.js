776113a28f4ae63fc6fb0d907bd6865719d81054
Jason Huntrods <jason@iastate.edu>
2016-01-25 17:16:38 -0600

    Update phantom.js
    screenshot test

--- phantom.js
+++ phantom.js
@@ -1,21 +1,13 @@
-var page = require('webpage').create(),
-  system = require('system'),
-  t, address;
+var page = require('webpage').create();
+var system = require('system');
+var url = system.args[1];
+var selector = system.args[2];
 
-if (system.args.length === 1) {
-  console.log('Usage: loadspeed.js <some URL>');
-  phantom.exit();
-}
-
-t = Date.now();
-address = system.args[1];
-page.open(address, function(status) {
+page.open(url, function(status) {
   if (status !== 'success') {
-    console.log('FAIL to load the address');
+    console.log('Failed to load the address');
   } else {
-    t = Date.now() - t;
-    console.log('Loading ' + system.args[1]);
-    console.log('Loading time ' + t + ' msec');
+    page.render('seasons.png');
   }
   phantom.exit();
 });
