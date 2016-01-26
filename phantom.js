23e5a222703d37ed8faac4b4d6192a7895323ae2
Jason Huntrods <jason@iastate.edu>
2016-01-26 14:57:27 -0600

    Update phantom.js
    print

--- phantom.js
+++ phantom.js
@@ -2,7 +2,7 @@ var page = require('webpage').create();
 var system = require('system');
 var url = system.args[1];
 var meal = system.args[2];
-
+console.log(url + meal);
 page.open(url, function(meal) {
   page.render('public/seasons.png');
   var top = page.evaluate(function() {
