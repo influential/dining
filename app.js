6e249260c5d75d62d9612768f4ffe33f02d96945
Jason Huntrods <jason@iastate.edu>
2016-01-26 12:04:33 -0600

    Update app.js
    change dlisten

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
-app.listen(8080);
 
 app.get('/crop', function(req, res) {
   res.send('hello world');
@@ -10,3 +9,5 @@ app.get('/crop', function(req, res) {
 app.get('/image', function(req, res) {
   res.send('hello world');
 });
+
+app.listen(8080);
