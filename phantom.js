d467106651c6ce892b63860d51a91188df6dd03f
Jason Huntrods <jason@iastate.edu>
2016-01-26 15:54:23 -0600

    Update phantom.js
    changed render

--- phantom.js
+++ phantom.js
@@ -1,31 +1,24 @@
-/*var page = require('webpage').create();
-var system = require('system');
-var url = system.args[1];
-var meal = system.args[2];
-console.log(2);*/
 var page = require('webpage').create();
-page.open('http://google.com', function(status) {
-  console.log("Status: " + status);
-  if(status === "success") {
-    page.render('example.png');
-  }
-  phantom.exit();
-});
-/*page.open(url, function(meal) {
-  page.render('seasons.png');
+/*var system = require('system');
+var url = system.args[1];
+var meal = system.args[2];*/
+
+page.open(url, function() {
   var top = page.evaluate(function() {
     return document.querySelectorAll(".event-header")[0].getBoundingClientRect().top;
   });
-  console.log(top);*/
-  /*var bottom;
+  console.log(top);
+  var bottom;
   if(meal == 2) {
     bottom = page.evaluate(function() {
-      //return document.querySelectorAll(".event-header")[meal + 1].getBoundingClientRect().top;
+      return document.querySelectorAll(".event-header")[meal + 1].getBoundingClientRect().top;
     });
   } else {
     bottom = page.evaluate(function() {
       return document.querySelectorAll(".event-header")[meal + 1].getBoundingClientRect().top;
     });
   }
+  console.log(bottom);
+  page.render('public/seasons.png');
   phantom.exit();
-}, meal);*/
+});
