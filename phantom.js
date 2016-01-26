4e81fd249a80b1dba043407d4dfac9b11d6786a5
Jason Huntrods <jason@iastate.edu>
2016-01-26 14:51:54 -0600

    Update phantom.js
    change args again

--- phantom.js
+++ phantom.js
@@ -1,7 +1,7 @@
 var page = require('webpage').create();
 var system = require('system');
-var url = system.args[0];
-var meal = system.args[1];
+var url = system.args[1];
+var meal = system.args[2];
 
 page.open(url, function(meal) {
   page.render('public/seasons.png');
