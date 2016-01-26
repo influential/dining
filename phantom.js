e7dcae03d9bfcb1aaf1995b70ded486eec76bf35
Jason Huntrods <jason@iastate.edu>
2016-01-26 13:38:46 -0600

    Update phantom.js
    organized phantom

--- phantom.js
+++ phantom.js
@@ -1,21 +1,22 @@
 var page = require('webpage').create();
 var system = require('system');
 var url = system.args[1];
-var selector = system.args[2];
+var meal = system.args[2];
 
 page.open(url, function(status) {
-  if (status !== 'success') {
-    console.log('Failed to load the address');
-  } else {
-    page.render('assets/images/seasons.png');
+  page.render('public/seasons.png');
   var top = page.evaluate(function() {
-    return document.querySelectorAll(".event-header")[1].getBoundingClientRect().top;
+    return document.querySelectorAll(".event-header")[meal].getBoundingClientRect().top;
   });
-  console.log(top);
-  var bottom = page.evaluate(function() {
-    return document.querySelectorAll(".event-header")[2].getBoundingClientRect().top;
-  });
-  console.log(bottom);
+  var bottom;
+  if(meal == 2) {
+    bottom = page.evaluate(function() {
+      //return document.querySelectorAll(".event-header")[meal + 1].getBoundingClientRect().top;
+    });
+  } else {
+    bottom = page.evaluate(function() {
+      return document.querySelectorAll(".event-header")[meal + 1].getBoundingClientRect().top;
+    });
   }
   phantom.exit();
 });
