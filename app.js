e7890f989712dbe811022e454301f62f1e924c7b
Jason Huntrods <jason@iastate.edu>
2016-01-26 17:05:11 -0600

    Update app.js
    let's try again

--- app.js
+++ app.js
@@ -50,20 +50,18 @@ function run() {
 function screenshot(location, meal) {
   var date = new Date().toISOString().slice(0,10);
   var url = 'http://dining.iastate.edu/menus/' + location + '/' + date;
-  var childArgs = ['/root/dining/phantom.js', url, meal];
-  console.log(url + "---" + meal);
+  var childArgs = ['/root/dining/phantom.js', url, meal, location];
   childProcess.execFile(phantomjs.path, childArgs, function(err, stdout, stderr) {
   	if(err) console.log(err.toString());
 	if(stderr) console.log(stderr.toString());
 	if(stdout) console.log(stdout.toString());
-	/*console.log(stdout.toString());
-	var results = stdout.toString().split("\n");
-	console.log(results[0] + "---" + results[1]);*/
-	/*gm('/root/dining/public/' + location + '.png').crop(1000, parseInt(results[1]) - parseInt(results[0]), 0, parseInt(results[0]))
+	var results = stdout.toString().split("---");
+	console.log(results[0] + "-" + results[1]);
+	gm('/root/dining/public/' + location + '.png').crop(1000, parseInt(results[1]) - parseInt(results[0]), 0, parseInt(results[0]))
 	.write('/root/dining/public/' + location + '.png', function (err) {
 		if(err) console.log(err);
 		return 1;
-	});*/
+	});
   });
 }
 
