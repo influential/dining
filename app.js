c569b373f3416469dc858ea3b3600f5acd4c661e
Jason Huntrods <jason@iastate.edu>
2016-01-26 13:02:31 -0600

    Update app.js
    added auth router

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
+var controllers = require('/controllers');
+
 app.use(express.static(__dirname + '/public'));
 
-app.get('/crop', function(req, res) {
-  res.send('hello world');
-});
+router.get('/auth', controllers.auth);
 
 app.get('/image', function(req, res) {
   res.send('hello world');
