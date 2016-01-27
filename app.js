91271557a1319aca65b40349da04eb728d2e67ee
Jason Huntrods <jason@iastate.edu>
2016-01-27 13:50:16 -0600

    Update app.js
    fixed

--- app.js
+++ app.js
@@ -161,43 +161,4 @@ function confirm() {
 	});
 }
 
-var twitterAPI = require('node-twitter-api');
-
-module.exports = {
-	
-	connect: function(req, res) {
-		
-	var twitter = new twitterAPI({
-	    consumerKey: sails.config.oauth.CK,
-	    consumerSecret: sails.config.oauth.CKS,
-	    callback: 'http://104.131.2.65/twitter'
-	});
-	if(req.param('oauth_verifier')) {
-		twitter.getAccessToken(process.env.RT, process.env.RTS, req.param('oauth_verifier'), function(error, accessToken, accessTokenSecret, results) {
-			if (error) {
-				console.log(error);
-			} else {
-				return res.send("AccessToken: " + accessToken + "\nAccessTokenSecret: " + accessTokenSecret);
-			}
-		});
-	} else {
-		twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results) {
-			if(error) {
-				console.log('Error getting OAuth request token : ' + error);
-			} else {
-				req.session.RT = requestToken;
-				req.session.RTS = requestTokenSecret;
-				var url = "https://twitter.com/oauth/authenticate?oauth_token=" + requestToken;
-				res.redirect(url);
-			}
-		});
-	}
-	
-	},
-	
-	test: function(req, res) {
-		SchedulingService.snapshot('seasons', '.active-trail');
-	}
-	
-};
 }
