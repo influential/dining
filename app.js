2946040a8185fd89cad95a30d88e7812ae752f89
Jason Huntrods <jason@iastate.edu>
2016-01-28 13:32:30 -0600

    Update app.js
    error add ins up to upload

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ var keys = require('./local.js');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.listen(3000);
-app.get('/tweet', function(req, res) { menu(0) /* res.send(200) */ });
+app.get('/est', function(req, res) { menu(0) });
+app.get('/tweet', function(req, res) { res.send(200) });
 app.get('/twitter', function(req, res) { authenticate() });
 app.get('/auth', function(req, res) { confirm(req) });
-
 //run();
 
 /* Cron Jobs */
@@ -24,19 +24,13 @@ app.get('/auth', function(req, res) { confirm(req) });
 function run() {
   
     //Breakfast
-    new CronJob('00 30 06 * * 1-5', function() {
-        menu(0);
-    }, null, true, 'America/Chicago');
+    new CronJob('00 30 06 * * 1-5', function() { menu(0) }, null, true, 'America/Chicago');
 
     //Lunch
-    new CronJob('00 00 10 * * 0-6', function() {
-        menu(1);
-    }, null, true, 'America/Chicago');
+    new CronJob('00 00 10 * * 0-6', function() { menu(1) }, null, true, 'America/Chicago');
 
     //Dinner
-    new CronJob('00 00 04 * * 0-6', function() {
-        menu(2);
-    }, null, true, 'America/Chicago');
+    new CronJob('00 00 04 * * 0-6', function() { menu(2) }, null, true, 'America/Chicago');
   
 }
 
@@ -47,9 +41,16 @@ function screenshot(location, meal, cb) {
     var url = 'http://dining.iastate.edu/menus/' + location + '/' + date;
     var childArgs = ['/root/dining/phantom.js', url, meal, location];
     childProcess.execFile(phantomjs.path, childArgs, function(err, stdout, stderr) {
-        var results = stdout.toString().split("---");
-        gm('/root/dining/public/' + location + '.png').crop(1000, parseInt(results[1]) - parseInt(results[0]), 0, parseInt(results[0]))
-        .write('/root/dining/public/' + location + '.png', cb);
+    	if(err) return screenshot(location, meal, cb);
+        save(stdout.toString().split("---"), location, cb);
+    });
+}
+
+function save(results, location, cb) {
+	gm('/root/dining/public/' + location + '.png').crop(1000, parseInt(results[1]) - parseInt(results[0]), 0, parseInt(results[0]))
+    .write('/root/dining/public/' + location + '.png', function(err) {
+   		if(err) return save(results, location, cb);
+   		cb();
     });
 }
 
@@ -61,8 +62,9 @@ function join() {
         function(cb) { gm('/root/dining/public/seasons-title.png').append('/root/dining/public/seasons.png').write('/root/dining/public/seasons.png', cb) },
         function(cb) { gm('/root/dining/public/conversations-title.png').append('/root/dining/public/conversations.png').write('/root/dining/public/conversations.png', cb) },
         function(cb) { gm('/root/dining/public/storms-title.png').append('/root/dining/public/storms.png').write('/root/dining/public/storms.png', cb) },
-    ], function() {
-    	tweet();
+    ], function(err) {
+    	if(err) return join();
+    	post();
     });
 }
 
@@ -138,27 +140,30 @@ function menu(meal) {
     }
 }
 
+/* Upload Pictures */
+
+function post() {
+    var twitter = new twitterAPI({ consumerKey: keys.oauth.CK, consumerSecret: keys.oauth.CKS, callback: 'http://104.131.2.65:3000/tweet' });
+	async.parallel([
+		function(cb) { return twitter.uploadMedia({media: '/root/dining/public/udm.png'}, keys.oauth.AT, keys.oauth.ATS, cb) },
+		function(cb) { return twitter.uploadMedia({media: '/root/dining/public/storms.png'}, keys.oauth.AT, keys.oauth.ATS, cb) },
+		function(cb) { return twitter.uploadMedia({media: '/root/dining/public/conversations.png'}, keys.oauth.AT, keys.oauth.ATS, cb) },
+		function(cb) { return twitter.uploadMedia({media: '/root/dining/public/storms.png'}, keys.oauth.AT, keys.oauth.ATS, cb) }
+    ], function(err, results) {
+    	if(err) return post();
+		var ids = results.map(function(obj) { return obj[0].media_id });
+		console.log(ids);
+		//tweet(ids);
+  	});
+}
+
 /* Tweet Pictures */
 
-function tweet() {
-	console.log("tweet");
-	return;
-    var twitter = new twitterAPI({ consumerKey: keys.oauth.CK, consumerSecret: keys.oauth.CKS, callback: 'http://104.131.2.65:3000/tweet' });
-	var actions;
-	async.parallel([
-    		function(cb) { return twitter.uploadMedia({media: '/root/dining/public/udm.png'}, keys.oauth.AT, keys.oauth.ATS, cb) },
-    		function(cb) { return twitter.uploadMedia({media: '/root/dining/public/storms.png'}, keys.oauth.AT, keys.oauth.ATS, cb) },
-    		function(cb) { return twitter.uploadMedia({media: '/root/dining/public/conversations.png'}, keys.oauth.AT, keys.oauth.ATS, cb) },
-    		function(cb) { return twitter.uploadMedia({media: '/root/dining/public/storms.png'}, keys.oauth.AT, keys.oauth.ATS, cb) }
-        ], function(err, results) {
-    		var ids = results.map(function(obj) { return obj[0].media_id });
-    		console.log(ids);
-    		twitter.statuses("update", {media_ids: ids}, keys.oauth.AT, keys.oauth.ATS, function(err, data, response) {
-		        	if (err) console.log(err);
-		        	console.log("tweeted");
-		            cb();
-	        });
-  	});
+function tweet(ids) {
+	twitter.statuses("update", {media_ids: ids}, keys.oauth.AT, keys.oauth.ATS, function(err, data, response) {
+		if(err) return tweet(ids);
+    	notify(true);
+    });
 }
 
 /* Oauth Authentication */
@@ -180,4 +185,10 @@ function confirm(req) {
 	twitter.getAccessToken(process.env.RT, process.env.RTS, req.query.oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
 		console.log("AccessToken: " + accessToken + "\nAccessTokenSecret: " + accessTokenSecret);
 	});
+}
+
+/* Status Notification */
+
+function notify(success) {
+	if(success) console.log("notify");
 }
