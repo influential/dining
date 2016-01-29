beafecfeaf6e891f766ad0788f8ea57fc070a61a
Jason Huntrods <jason@iastate.edu>
2016-01-29 12:43:00 -0600

    Update app.js
    test post

--- app.js
+++ app.js
@@ -14,10 +14,14 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.get('/tweet', function(req, res) { res.send(200) });
 app.get('/test', function(req, res) { menu(1) });
-//app.get('/twitter', function(req, res) { authenticate() });
+//app.get('/login', function(req, res) { authenticate() });
 //app.get('/auth', function(req, res) { confirm(req) });
 app.listen(3000);
 run();
+
+/* Global Twitter Object For Tweeting */
+
+var twitter = new twitterAPI({ consumerKey: keys.oauth.CK, consumerSecret: keys.oauth.CKS, callback: 'http://104.131.2.65:3000/tweet' });
 
 /* Cron Jobs */
 
@@ -29,7 +33,7 @@ function run() {
 
 /* Screenshots/Crops Menu Page and Uploads to Twitter */
 
-function screenshot(location, meal, twitter, cb) {
+function screenshot(location, meal, cb) {
     var date = new Date().toISOString().slice(0,10);
     var url = 'http://dining.iastate.edu/menus/' + location + '/' + date;
     var childArgs = ['/root/dining/phantom.js', url, meal, location];
@@ -52,34 +56,33 @@ function screenshot(location, meal, twitter, cb) {
 
 function menu(meal) {
     var day = new Date().getDay();
-    var twitter = new twitterAPI({ consumerKey: keys.oauth.CK, consumerSecret: keys.oauth.CKS, callback: 'http://104.131.2.65:3000/tweet' });
     var breakfast = [
-        function(cb) { screenshot("udm", 0, twitter, cb) },
-    	function(cb) { screenshot("seasons", 0, twitter, cb) },
-    	function(cb) { screenshot("conversations", 0, twitter, cb) }
+        function(cb) { screenshot("udm", 0, cb) },
+    	function(cb) { screenshot("seasons", 0, cb) },
+    	function(cb) { screenshot("conversations", 0, cb) }
     ];
     var lunch = [
-        function(cb) { screenshot("udm", 1, twitter, cb) },
-    	function(cb) { screenshot("seasons", 1, twitter, cb) },
-    	function(cb) { screenshot("conversations", 1, twitter, cb) }
+        function(cb) { screenshot("udm", 1, cb) },
+    	function(cb) { screenshot("seasons", 1, cb) },
+    	function(cb) { screenshot("conversations", 1, cb) }
     ];
     var dinner = [
-        function(cb) { screenshot("udm", 2, twitter, cb) },
-    	function(cb) { screenshot("seasons", 2, twitter, cb) },
-    	function(cb) { screenshot("conversations", 2, twitter, cb) },
-    	function(cb) { screenshot("storms", 2, twitter, cb) }
+        function(cb) { screenshot("udm", 2, cb) },
+    	function(cb) { screenshot("seasons", 2, cb) },
+    	function(cb) { screenshot("conversations", 2, cb) },
+    	function(cb) { screenshot("storms", 2, cb) }
     ];
     if(meal == 0) {
         async.parallel(breakfast, function(err, results) {
         	var ids = results.map(function(obj) { return obj[0].media_id_string });
-			tweet(twitter, ids.join(), meal);
+			tweet(ids.join(), meal);
         });
     } else if(meal == 1) {
     	if(day == 0 || day == 6) lunch = lunch.slice(0,2);
         async.parallel(lunch, function(err, results) {
         	var ids = results.map(function(obj) { return obj[0].media_id_string });
         	console.log(ids);
-			//tweet(twitter, ids.join(), meal);
+			tweet(ids.join(), meal);
         });
     } else {
         if(day == 0) dinner = dinner.slice(0,2);
@@ -87,14 +90,14 @@ function menu(meal) {
         else if(day == 6) dinner = dinner.splice(2, 1);
         async.parallel(dinner, function(err, results) {
         	var ids = results.map(function(obj) { return obj[0].media_id_string });
-			tweet(twitter, ids.join(), meal);
+			tweet(ids.join(), meal);
         });
     }
 }
 
 /* Tweet Pictures */
 
-function tweet(twitter, ids, meal) {
+function tweet(ids, meal) {
 	var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 	var day = days[new Date().getDay()];
 	var text = " Breakfast";
@@ -106,11 +109,18 @@ function tweet(twitter, ids, meal) {
     });
 }
 
+/* Status Notification */
+
+function notify(success) {
+	if(success) console.log("notify");
+	else console.log("fail");
+}
+
 /* Oauth Authentication */
 
 function authenticate(res) {
-	var twitter = new twitterAPI({ consumerKey: keys.oauth.CK, consumerSecret: keys.oauth.CKS, callback: 'http://104.131.2.65:3000/auth' });
-	twitter.getRequestToken(function(err, requestToken, requestTokenSecret, results) {
+	var twitterAuth = new twitterAPI({ consumerKey: keys.oauth.CK, consumerSecret: keys.oauth.CKS, callback: 'http://104.131.2.65:3000/auth' });
+	twitterAuth.getRequestToken(function(err, requestToken, requestTokenSecret, results) {
 		if(err) console.log(err);
 		process.env.RT = requestToken;
 		process.env.RTS = requestTokenSecret;
@@ -122,16 +132,9 @@ function authenticate(res) {
 /* Oauth Confirmation */
 
 function confirm(req) {
-	var twitter = new twitterAPI({ consumerKey: keys.oauth.CK, consumerSecret: keys.oauth.CKS, callback: 'http://104.131.2.65:3000/auth' });
-	twitter.getAccessToken(process.env.RT, process.env.RTS, req.query.oauth_verifier, function(err, accessToken, accessTokenSecret, results) {
+	var twitterConfirm = new twitterAPI({ consumerKey: keys.oauth.CK, consumerSecret: keys.oauth.CKS, callback: 'http://104.131.2.65:3000/auth' });
+	twitterConfirm.getAccessToken(process.env.RT, process.env.RTS, req.query.oauth_verifier, function(err, accessToken, accessTokenSecret, results) {
 		if(err) console.log(err);
 		console.log("AccessToken: " + accessToken + "\nAccessTokenSecret: " + accessTokenSecret);
 	});
-}
-
-/* Status Notification */
-
-function notify(success) {
-	if(success) console.log("notify");
-	else console.log("fail");
 }
