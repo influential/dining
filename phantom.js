7149e4952bd77c487f251b308f0cd19d09dafbe8
Jason Huntrods <jason@iastate.edu>
2016-02-01 16:41:23 -0600

    Update phantom.js
    added closed handling

--- phantom.js
+++ phantom.js
@@ -5,43 +5,51 @@ var meal = system.args[2];
 var save = system.args[3];
 
 page.viewportSize = {width: 390, height: 2500};
+
 page.open(url, function(status) {
-  if(save == 'seasons') page.render('/root/dining/public/seasons.png');
-  if(save == 'conversations') page.render('/root/dining/public/conversations.png');
-  if(save == 'udm') page.render('/root/dining/public/udm.png');
-  if(save == 'storms') page.render('/root/dining/public/storms.png');
-  var top, bottom;
-  if(meal == 0) {
-    top = page.evaluate(function() {
-      return document.querySelectorAll(".event-header")[0].getBoundingClientRect().top;
+    if(save == 'seasons') page.render('/root/dining/public/seasons.png');
+    if(save == 'conversations') page.render('/root/dining/public/conversations.png');
+    if(save == 'udm') page.render('/root/dining/public/udm.png');
+    if(save == 'storms') page.render('/root/dining/public/storms.png');
+    var closed = page.evaluate(function() {
+        return document.querySelectorAll(".event-header").length;
     });
-    bottom = page.evaluate(function() {
-      return document.querySelectorAll(".event-header")[1].getBoundingClientRect().top;
-    });
-  } else if(meal == 1) {
-    top = page.evaluate(function() {
-      var check = document.querySelectorAll(".event-header").length;
-      if(check == 2) return document.querySelectorAll(".event-header")[0].getBoundingClientRect().top;
-      else return document.querySelectorAll(".event-header")[1].getBoundingClientRect().top;
-    });
-    bottom = page.evaluate(function() {
-      var check = document.querySelectorAll(".event-header").length;
-      if(check == 2) return document.querySelectorAll(".event-header")[1].getBoundingClientRect().top;
-      else return document.querySelectorAll(".event-header")[2].getBoundingClientRect().top;
-    });
-  } else {
-    top = page.evaluate(function() {
-      var check = document.querySelectorAll(".event-header").length;
-      if(check == 1) return document.querySelectorAll(".event-header")[0].getBoundingClientRect().top;
-      if(check == 2) return document.querySelectorAll(".event-header")[1].getBoundingClientRect().top;
-      else return document.querySelectorAll(".event-header")[2].getBoundingClientRect().top;
-    });
-    bottom = page.evaluate(function() {
-      var check = document.querySelectorAll(".event-header").length;
-      if(check > 3) return document.querySelectorAll(".event-header")[3].getBoundingClientRect().top;
-      else return document.querySelectorAll(".legend")[0].getBoundingClientRect().top;
-    });
-  }
-  console.log(top + "---" + bottom);
-  phantom.exit();
+    if(closed < 1) {
+        console.log("---");
+        phantom.exit();
+    }
+    var top, bottom;
+    if(meal == 0) {
+        top = page.evaluate(function() {
+            return document.querySelectorAll(".event-header")[0].getBoundingClientRect().top;
+        });
+        bottom = page.evaluate(function() {
+            return document.querySelectorAll(".event-header")[1].getBoundingClientRect().top;
+        });
+    } else if(meal == 1) {
+        top = page.evaluate(function() {
+            var check = document.querySelectorAll(".event-header").length;
+            if(check == 2) return document.querySelectorAll(".event-header")[0].getBoundingClientRect().top;
+            else return document.querySelectorAll(".event-header")[1].getBoundingClientRect().top;
+        });
+        bottom = page.evaluate(function() {
+            var check = document.querySelectorAll(".event-header").length;
+            if(check == 2) return document.querySelectorAll(".event-header")[1].getBoundingClientRect().top;
+            else return document.querySelectorAll(".event-header")[2].getBoundingClientRect().top;
+        });
+    } else {
+        top = page.evaluate(function() {
+            var check = document.querySelectorAll(".event-header").length;
+            if(check == 1) return document.querySelectorAll(".event-header")[0].getBoundingClientRect().top;
+            if(check == 2) return document.querySelectorAll(".event-header")[1].getBoundingClientRect().top;
+            else return document.querySelectorAll(".event-header")[2].getBoundingClientRect().top;
+        });
+        bottom = page.evaluate(function() {
+            var check = document.querySelectorAll(".event-header").length;
+            if(check > 3) return document.querySelectorAll(".event-header")[3].getBoundingClientRect().top;
+            else return document.querySelectorAll(".legend")[0].getBoundingClientRect().top;
+        });
+    }
+    console.log(top + "---" + bottom);
+    phantom.exit();
 });
